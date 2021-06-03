5cafc8bb3ea4f9140eeac59356b5b56caa2383e5
Alex An <dafigav9797@gmail.com>
2021-06-04 01:00:31 +0700

    asd

--- lab3/Program.cs
+++ lab3/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 class Matrix
 {
-    int[,] mat = new int[5, 5];
+    public int[,] mat = new int[5, 5];
     public Matrix()
     {
         mat = new int[5, 5];
@@ -36,7 +36,7 @@ class Matrix
         string strMatrix = "";
         for (int i = 0; i < 5; i++)
         {
-            string str = "";
+            string str = " ";
             for (int j = 0; j < 4; j++)
             {
                 str += mat[i, j].ToString() + "  ";
@@ -57,11 +57,13 @@ class Matrix
         return (obj is Matrix) && this.Equals((Matrix)obj);
     }
 
+    // проверка на равенство заданного объекта текущему
     public bool Equals(Matrix m)
     {
         return mat == m.mat;
     }
 
+    //получение хэш-кода
     public override int GetHashCode()
     {
         return mat.GetHashCode();
@@ -121,7 +123,35 @@ class Matrix
         return Value;
     }
 }
+// реализация прототипа с глубоким копированием
+class DeepCloneClass : Matrix, ICloneable
 
+{
+    public object Clone()
+    {
+        DeepCloneClass result = new DeepCloneClass();
+        for (int i = 0; i < 5; ++i)
+            for (int j = 0; j < 5; ++j)
+                result.mat[i, j] = this.mat[i, j];
+        return result; ;
+    }
+}
+// Пользовательский класс исключений
+public class InputException : System.Exception
+{
+    public InputException() : base()
+    {
+
+    }
+    public InputException(string message) : base(message)
+    {
+
+    }
+    public InputException(string message, System.Exception inner) : base(message, inner)
+    {
+
+    }
+}
 
 class Program
 {
@@ -132,9 +162,64 @@ class Program
         Matrix two = new Matrix();
         Console.WriteLine(two.ToString());
         _ = new Matrix();
-        Matrix three = one * two;
+        Matrix three = one + two;
         Console.WriteLine(three.ToString());
-        if (one != two) Console.WriteLine("Da");
+        if (one != two) Console.WriteLine("Neravni");
+
+        //пример работы реализации Прототипа
+        DeepCloneClass dc1 = new DeepCloneClass();
+        DeepCloneClass dc2 = (DeepCloneClass)dc1.Clone();
+        Console.WriteLine(dc1.ToString());
+        Console.WriteLine(dc2.ToString());
+
+        try
+        {
+            Console.WriteLine("Вводите значения 1 матрицы больше 0");
+            for (int i = 0; i < 5; ++i)
+                for (int j = 0; j < 5; ++j)
+                {
+                    one.mat[i, j] = Convert.ToInt32(Console.ReadLine());
+                    if (one.mat[i, j] <= 0) throw new InputException("Неправильный ввод");
+                }
+        }
+        catch (InputException ex)
+        {
+            Console.WriteLine("Ошибка: " + ex.Message);
+        }
+
+        try
+        {
+            Console.WriteLine("Значение 2 матрицы меньше 0");
+            for (int i = 0; i < 5; ++i)
+                for (int j = 0; j < 5; ++j)
+                {
+                    two.mat[i, j] = Convert.ToInt32(Console.ReadLine());
+                    if (two.mat[i, j] >= 0) throw new InputException("Неправильный ввод");
+                }
+        }
+        catch (InputException ex)
+        {
+            Console.WriteLine("Ошибка: " + ex.Message);
+        }
+
+        try
+        {
+            Console.WriteLine("Действие");
+            string act = Console.ReadLine();
+            if (act != "+" || act != "-" || act != "*")
+                throw new InputException("Неправильный ввод");
+            else
+            {
+                if (act == "+") three = one + two;
+                if (act == "-") three = one - two;
+                if (act == "*") three = one * two;
+            }
+        }
+        catch (InputException ex)
+        {
+            Console.WriteLine("Ошибка: " + ex.Message);
+        }
+        Console.WriteLine(three.ToString());
     }
 }
 
